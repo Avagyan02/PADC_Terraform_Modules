7769ad74ee2ae72048e77bdadbc403a682e4485e
Sam <samvelavagyan91@gmail.com>
2024-06-01 12:02:54 +0400

    init commit

--- main/provider.tf
+++ main/provider.tf
@@ -1,18 +1,3 @@
-locals {
-  region     = "eu-west-2"
-  access_key = "AKIAVRUVVTZBFB23W3HE"
-  secret_key = "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"
-}
-
-
-terraform {
-  backend "s3" {
-    bucket = "samvel-terraform-backend"
-    key    = "key/terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
-
 terraform {
   required_providers {
     aws = {
@@ -20,10 +5,4 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-
-provider "aws" {
-  region     = local.region
-  access_key = local.access_key
-  secret_key = local.secret_key
 }
